b7f9ce9f578fdac78cb2bf4a77b853034596c6f8
orestispanago <orestispanago@gmail.com>
2020-11-21 21:21:28 +0200

    Fixed typo in tat_gz.download_extract()

--- tar_gz.py
+++ tar_gz.py
@@ -4,7 +4,7 @@ import urllib.request
 url = "https://www.meso-star.com/projects/solstice/downloads/Solstice-0.9.0-GNU-Linux64.tar.gz"
 folder = "solstice"
 
-def download_extract(url, folder)
+
+def download_extract(url, folder):
     tar = tarfile.open(urllib.request.urlretrieve(url, filename=None)[0])
     tar.extractall(folder)
-
